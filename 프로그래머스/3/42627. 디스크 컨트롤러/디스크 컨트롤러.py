b9abf60886f7e8dd32b8f7142f3b52fdcd2ba482
yhyeonseo <154505487+franktome@users.noreply.github.com>
2025-07-03 20:37:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 28.02 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -10,16 +10,16 @@ def solution(jobs):
         if queue==[]:
             queue.append(heapq.heappop(jobs))
             current_time  = queue[0][0]
-        task = queue[0]
+        task = queue[-1]
         total_time+=(current_time + task[1]-task[0])
         current_time +=task[1]
-        queue.pop(0)
+        queue.pop()
         while(jobs):
             if jobs[0][0]<=current_time:
                 queue.append(heapq.heappop(jobs))
             else:
                 break
         if (queue):        
-            queue.sort(key = lambda x:(x[1],x[0]))
+            queue.sort(key = lambda x:(x[1],x[0]), reverse=True)
 
     return int(total_time/n)
