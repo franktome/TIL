a6abf2444208fec443fc8f671b5eb87b4b899d2b
yhyeonseo <154505487+franktome@users.noreply.github.com>
2025-07-04 18:03:40 +0900

    [level 3] Title: 단어 변환, Time: 0.57 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,36 +1,35 @@
 from collections import deque
 
-def compare(a,b):
+def check(a,b):
+    n=len(a)
     count=0
-    for i in range(len(a)):
-        if a[i] != b[i]:
+    for i in range(n):
+        if a[i]!=b[i]:
             count+=1
-    if count!=1:
-        return 0
-    else:
-        return 1
+    if count==1:
+        return True
+    return False
 
 def bfs(target, words, distances, queue):
     while(queue):
-        a = queue.popleft()
+        a=queue.popleft()
         if a==target:
             return
         for b in words:
-            if compare(a,b) and distances[b]==0:
-                distances[b] = distances[a]+1
+            if check(a,b) and distances[b]==-1:
+                distances[b]=distances[a]+1
                 queue.append(b)
-            
-def solution(begin, target, words):
-    queue = deque([begin])
-    # distances 딕셔너리로 관리해야 겠다.
-    print(target)
-    distances = {a:0 for a in words}
-    distances[begin]=0
-    print(distances)
-    if target not in words:
-        return 0
-    bfs(target, words, distances, queue)
     
-    answer = 0
-    answer = distances[target]
+
+def solution(begin, target, words):
+    distances={}
+    for word in words:
+        distances[word]=-1
+    distances[begin]=0
+    distances[target]=-1
+    queue = deque([begin])
+    bfs(target, words, distances, queue)
+    answer=0
+    if distances[target]!=-1:
+        answer=distances[target]
     return answer
