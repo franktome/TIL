c9c6965c9a171f97c87e14a6efd462ce5e763c2b
yhyeonseo <154505487+franktome@users.noreply.github.com>
2025-07-04 17:27:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.46 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,34 +1,21 @@
 from collections import deque
 
-def bfs(maps, distances, queue):
-    n=len(maps)
-    m=len(maps[0])
-    while(queue):
+def bfs(m,n, maps, distances, queue):
+    while queue:
         x,y = queue.popleft()
-        if (x+1<n and distances[x+1][y]==0 and maps[x+1][y]==1):
-            distances[x+1][y] = distances[x][y]+1
-            queue.append([x+1,y])
-        if (y+1<m and distances[x][y+1]==0 and maps[x][y+1]==1):
-            distances[x][y+1] = distances[x][y]+1
-            queue.append([x,y+1])
-        if (x-1>=0 and distances[x-1][y]==0 and maps[x-1][y]==1):
-            distances[x-1][y] = distances[x][y] +1
-            queue.append([x-1,y])
-        if (y-1>=0 and distances[x][y-1]==0 and maps[x][y-1]==1):
-            distances[x][y-1] = distances[x][y] +1
-            queue.append([x,y-1])
-        
+        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nx = x + dx
+            ny = y + dy
+            if 0<=nx<m and 0<=ny<n and maps[nx][ny]==1 and distances[nx][ny]==-1:
+                distances[nx][ny]=distances[x][y]+1
+                queue.append([nx,ny])
+
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
+    m = len(maps)
+    n = len(maps[0])
+    distances = [[-1]*n for _ in range(m)]
+    distances[0][0]=1
     queue = deque([[0,0]])
-    distances = [[0]*m for _ in range(n)]
-    distances[0][0] = 1
-    
-    bfs(maps,distances, queue)
-    answer = 0
-    if distances[n-1][m-1]==0:
-        answer=-1
-    else:
-        answer=distances[n-1][m-1]
+    bfs(m,n, maps, distances, queue)
+    answer = distances[m-1][n-1]
     return answer
