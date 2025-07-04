96fc114b012448bcfcbe991789dc1b3a416cff8e
yhyeonseo <154505487+franktome@users.noreply.github.com>
2025-07-04 16:57:28 +0900

    [level 3] Title: 네트워크, Time: 2.88 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 9.29 MB, 시간: 2.39 ms
+메모리: 9.32 MB, 시간: 2.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 26일 16:37:49
+2025년 07월 04일 16:57:25
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,19 @@
 from collections import deque
 
-def dfs(computers, visited, start):
-    stack = deque([start])
-    while stack:
-        j = stack.popleft()
-        if visited[j]==0:
-            visited[j]=1
-        for i in range(len(computers)):
-            if visited[i]==0 and computers[j][i]==1:
-                stack.append(i)
-    
+def dfs(n, computers, visited, start):
+    queue=deque([start])
+    while queue:
+        a=queue.popleft()
+        visited[a]=1
+        for idx,b in enumerate(computers[a]):
+            if visited[idx]==-1 and b==1:
+                queue.append(idx)
+
 def solution(n, computers):
-    visited=[0]*n
     answer = 0
-    i=0
-    while 0 in visited:
-        if visited[i]==0:
-            visited[i]=1
-            dfs(computers, visited, i)
+    visited = [-1]*n
+    for i in range(n):
+        if visited[i]==-1:
+            dfs(n,computers, visited, i)
             answer+=1
-        i+=1
-    
     return answer
