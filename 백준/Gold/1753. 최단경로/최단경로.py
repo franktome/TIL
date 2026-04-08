5121c902dd815d61010b88904717b108488a9312
Hyeonseo Yun <154505487+franktome@users.noreply.github.com>
2026-04-08 22:49:37 +0900

    [Gold IV] Title: 최단경로, Time: 412 ms, Memory: 135912 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,37 +1,31 @@
 import sys
 import heapq
-input=sys.stdin.readline
-inf = int(10e9)
+input = sys.stdin.readline
+inf = 10e9
 
 v,e = map(int, input().split())
-start = int(input())
-
-graph=[[] for _ in range(v+1)]
-distance=[inf for _ in range(v+1)]
-distance[start]=0
+s = int(input())
+graph = [[] for _ in range(v+1)]
+distances = [inf for _ in range(v+1)]
+distances[s]=0
 
 for i in range(e):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
+    a,b,w= map(int, input().split())
+    graph[a].append((w,b))
 
-q=[]
-heapq.heappush(q,(0,start))
+def dijkstra(start, graph, distances):
+    q=[]
+    heapq.heappush(q,(0,start)) # 여기서 heap에 넣어주는 값 중에서 첫 번쩨 값은 distance의 가중치를 의미하네. 이걸로 정렬해야 해서 distance가 있음에도 다시 넣어주네.
+    while(q):
+        w, n = heapq.heappop(q)
+        for w2,n2 in graph[n]:
+            if distances[n2]> distances[n]+w2:
+                distances[n2] = distances[n]+w2
+                heapq.heappush(q,(distances[n]+w2,n2))
 
-while(q):
-    d, n = heapq.heappop(q)
-
-    if distance[n]<d:
-        continue
-
-    for i in graph[n]:
-        cost = d + i[1]
-        if cost<distance[i[0]]:
-            distance[i[0]] = cost
-            heapq.heappush(q,(cost,i[0]))
-
-
+dijkstra(s,graph,distances)
 for i in range(1,v+1):
-    if distance[i]==inf:
+    if distances[i]==inf:
         print("INF")
     else:
-        print(distance[i])
+        print(distances[i])
