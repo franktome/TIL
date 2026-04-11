41f91c12579435134c19dcd2458e3372d72180b6
Hyeonseo Yun <154505487+franktome@users.noreply.github.com>
2026-04-11 10:50:55 +0900

    [Gold IV] Title: 플로이드, Time: 156 ms, Memory: 111632 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,29 +1,33 @@
 import sys
 input = sys.stdin.readline
-inf = int(10e9)
+inf = 10e9
 
-n = int(input())
-m = int(input())
-graph=[[inf]*(n+1) for _ in range(n+1)]
-
-for _ in range(m):
-    a,b,c = map(int, input().split())
-    graph[a][b] = min(graph[a][b],c)  # 같은 노드를 잇는 여러개의 간선이 있을 수 있다.
-
-for i in range(n+1):
-    for j in range(n+1):
-        if i==j:
-            graph[i][j]=0
-
-for k in range(1,n+1):
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+n = int(input().strip())
+m = int(input().strip())
+graph = [[inf]*(n+1) for _ in range(n+1)]
+for i in range(m):
+    a,b,c = map(int,input().split())
+    graph[a][b]=min(graph[a][b],c)
 
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if graph[i][j]==inf:
-            print(0,end=" ")
+        if i==j:
+            graph[i][j]=0
+
+def bellman_ford():
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if graph[i][j]> graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
+
+bellman_ford()
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if graph[i][j] == inf:
+            print(0,end=' ')
         else:
-            print(graph[i][j], end=" ")
+            print(graph[i][j], end=' ')
     print()
+
+        
